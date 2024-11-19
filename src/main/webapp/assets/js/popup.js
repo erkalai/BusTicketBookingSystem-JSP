e4726568639524d0bac76231b45cb6e98b527db7
erkalai <er.kalai.v@gmail.com>
2024-11-19 19:00:44 +0530

    Add Station Popup

---
+++ src/main/webapp/assets/js/popup.js
@@ -0,0 +1,8 @@
+// Popup Control
+        document.getElementById("button").addEventListener("click", function(){
+            document.querySelector(".popup").style.display = "flex";
+        })
+        document.querySelector(".close").addEventListener("click", function(){
+            document.querySelector(".popup").style.display = "none";
+        })
+    
